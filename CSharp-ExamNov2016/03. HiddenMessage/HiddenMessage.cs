3ac39d699efcf361980fc0b688c79dab67df963a
Irina Hristova <hristova119@gmail.com>
2017-08-08 12:39:25 +0300

    Exam.

--- CSharp-ExamNov2016/03. HiddenMessage/HiddenMessage.cs
+++ CSharp-ExamNov2016/03. HiddenMessage/HiddenMessage.cs
@@ -10,6 +10,34 @@ namespace _03.HiddenMessage
     {
         static void Main(string[] args)
         {
+            string hiddenMessage = "";
+            string indStr = Console.ReadLine();
+
+            int i = 0;
+            int s = 0;
+            string message = "";
+
+
+            while (indStr != "end")
+            {
+                i = int.Parse(indStr);
+                s = int.Parse(Console.ReadLine());
+                message = Console.ReadLine();
+                indStr = Console.ReadLine();
+
+
+                for (int j = i; i < message.Length; i += s)
+                {
+                    hiddenMessage += message[i];
+                }
+
+            }
+
+
+            //Console.WriteLine(i);
+            //Console.WriteLine(s);
+            //Console.WriteLine(message);
+            Console.WriteLine(hiddenMessage);
 
         }
     }
