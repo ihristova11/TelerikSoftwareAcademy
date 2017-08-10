137d238e4cfe6cc16e9427d53285f9127eb2caec
Irina Hristova <hristova119@gmail.com>
2017-08-11 00:15:38 +0300

    Homework.

---
+++ C#-Advanced/03. Methods/11. AddingPolynomials/AddingPolynomials.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+namespace _11.AddingPolynomials
+{
+    class AddingPolynomials
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] coeff1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] coeff2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Sum(coeff1, coeff2, n);
+        }
+
+        static void Sum(int[] arr1, int[] arr2, int n)
+        {
+            int[] sum = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                sum[i] = arr1[i] + arr2[i];
+            }
+
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
