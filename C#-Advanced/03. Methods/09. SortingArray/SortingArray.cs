033412e07116aae84b42cb3affa13671ebe89362
Irina Hristova <hristova119@gmail.com>
2017-08-10 23:45:36 +0300

    Homework.

---
+++ C#-Advanced/03. Methods/09. SortingArray/SortingArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.SortingArray
+{
+    class SortingArray
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            SortArray(numbers, n);
+        }
+
+        static void SortArray(int[] arr, int length)
+        {
+            //int[] sorted = new int[length];
+            int temp = 0;
+
+            for (int write = 0; write < arr.Length; write++)
+            {
+                for (int sort = 0; sort < arr.Length - 1; sort++)
+                {
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        temp = arr[sort + 1];
+                        arr[sort + 1] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
