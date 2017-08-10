263ccdc307ad19af4393a9cbbd88995d2023b0cc
Irina Hristova <hristova119@gmail.com>
2017-08-10 19:15:12 +0300

    C# Advanced Homework.

--- C#-Advanced/03. Methods/06. FirstLargerThanNeighbours/FirstLargerThanNeighbours.cs
+++ C#-Advanced/03. Methods/06. FirstLargerThanNeighbours/FirstLargerThanNeighbours.cs
@@ -6,6 +6,27 @@ namespace _06.FirstLargerThanNeighbours
     {
         static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+            string[] numbers = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(FirstLarger(n, numbers));
+            //Console.WriteLine((int.Parse(numbers[i]) > int.Parse(numbers[i - 1])) && (int.Parse(numbers[i]) < int.Parse(numbers[i + 1])));
+        }
+
+        static int FirstLarger(int n, string[] numbers)
+        {
+            int index = -1;
+            for (int i = 1; i < n - 1; i++)
+            {
+                if ((int.Parse(numbers[i - 1]) < int.Parse(numbers[i])) && (int.Parse(numbers[i]) > int.Parse(numbers[i + 1])))
+                {
+                    index = i;
+                    break;
+                }
+                //Console.WriteLine(((int.Parse(numbers[i - 1]) < int.Parse(numbers[i])) && (int.Parse(numbers[i]) > int.Parse(numbers[i + 1]))));
+            }
+
+            return index;
         }
     }
 }
---
+++ C#-Advanced/03. Methods/07. ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace _07.ReverseNumber
+{
+    class ReverseNumber
+    {
+        static void Main()
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            ReverseIt(n);
+        }
+
+        static void ReverseIt(decimal number)
+        {
+
+            char[] digits = number.ToString().ToCharArray();
+            char[] reversedNum = new char[digits.Length];
+
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                reversedNum[digits.Length - i - 1] = digits[i];
+                //Console.WriteLine(reversedNum[i]);
+            }
+
+            Console.WriteLine(string.Join("", reversedNum));
+        }
+    }
+}
