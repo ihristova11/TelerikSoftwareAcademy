667ac39266f8fcd7f1ea7cdbb33b4502f7e9fcaa
Irina Hristova <hristova119@gmail.com>
2017-08-11 00:05:58 +0300

    Homework.

--- C#-Advanced/03. Methods/10. NFactorial/NFactorial.cs
+++ C#-Advanced/03. Methods/10. NFactorial/NFactorial.cs
@@ -1,32 +1,25 @@
 ﻿using System;
-
+using System.Linq;
+using System.Numerics;
 namespace _10.NFactorial
 {
     class NFactorial
     {
         static void Main()
         {
-            char[] line = Console.ReadLine().ToCharArray();
-            //Console.WriteLine(Fact(number));
-            //Console.WriteLine(line[1]);
-            string num = "";
-            for (int i = 0; i < line.Length; i++)
-            {
-                num += line[i];
-            }
-
-            Console.WriteLine(Fact(int.Parse(num)));
+            int n = int.Parse(Console.ReadLine());
+            Fact(n);
         }
 
-        static int Fact(int number)
+        static void Fact(int number)
         {
-            int fact = 1;
+            BigInteger fact = 1;
             for (int i = 1; i < number + 1; i++)
             {
                 fact *= i;
             }
 
-            return fact;
+            Console.WriteLine(fact);
         }
     }
 }
