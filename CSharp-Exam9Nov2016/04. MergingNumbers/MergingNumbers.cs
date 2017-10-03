4b27c45df4d204e3c6fd5a350eb6cefc13a6c9ee
Irina Hristova <hristova119@gmail.com>
2017-10-03 22:55:08 +0300

    Solved new problem.

---
+++ CSharp-Exam9Nov2016/04. MergingNumbers/MergingNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.MergingNumbers
+{
+    class MergingNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            string summing = "";
+            string merging = "";
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                merging += (numbers[i - 1] % 10).ToString();
+                merging += (numbers[i] / 10).ToString() + " ";
+                summing += (numbers[i - 1] + numbers[i]).ToString() + " ";
+            }
+            Console.WriteLine(merging);
+            Console.WriteLine(summing);
+        }
+    }
+}
