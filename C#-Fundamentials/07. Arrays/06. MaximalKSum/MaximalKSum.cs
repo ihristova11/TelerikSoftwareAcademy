f9defab9d8d01576786b1788408ab5ea7b6940d4
Irina Hristova <hristova119@gmail.com>
2017-10-04 17:04:07 +0300

    Solved problem 06.

--- C#-Fundamentials/07. Arrays/06. MaximalKSum/MaximalKSum.cs
+++ C#-Fundamentials/07. Arrays/06. MaximalKSum/MaximalKSum.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace _06.MaximalKSum
 {
-    class Program
+    class MaximalKSum
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            for(int j = 0; j < k; j++)
+            {
+                sum += arr[j];
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
