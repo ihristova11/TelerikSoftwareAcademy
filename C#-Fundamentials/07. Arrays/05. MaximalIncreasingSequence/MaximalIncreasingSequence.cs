8014c05a0003bc75044af2b2e0250584100ee2b3
Irina Hristova <hristova119@gmail.com>
2017-10-04 16:50:09 +0300

    Added solution to 05.

---
+++ C#-Fundamentials/07. Arrays/05. MaximalIncreasingSequence/MaximalIncreasingSequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.MaximalIncreasingSequence
+{
+    class MaximalIncreasingSequence
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int prev, curr_len = 1, max_len = 0;
+
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                prev = x;
+                x = int.Parse(Console.ReadLine());
+                if (prev < x)
+                {
+                    curr_len++;
+                    max_len = (max_len < curr_len) ? curr_len : max_len;
+                }
+                else
+                {
+                    curr_len = 1;
+                }
+            }
+            Console.WriteLine(max_len);
+        }
+    }
+}
