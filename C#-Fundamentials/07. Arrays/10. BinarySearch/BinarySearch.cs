9cea363fe2585766bdb3533759c4d40e826921fa
Irina Hristova <hristova119@gmail.com>
2017-10-04 22:13:37 +0300

    Added solution.

---
+++ C#-Fundamentials/07. Arrays/10. BinarySearch/BinarySearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.BinarySearch
+{
+    class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(Array.BinarySearch(arr, x)); 
+        }
+    }
+}
