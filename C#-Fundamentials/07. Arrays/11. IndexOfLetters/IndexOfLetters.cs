ea8bda54bf899702f0772cad228159d8ffa8534e
Irina Hristova <hristova119@gmail.com>
2017-10-04 23:05:46 +0300

    Solved problem 11.

--- C#-Fundamentials/07. Arrays/11. IndexOfLetters/IndexOfLetters.cs
+++ C#-Fundamentials/07. Arrays/11. IndexOfLetters/IndexOfLetters.cs
@@ -4,6 +4,16 @@ class IndexOfLetters
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        char[] letters = new char[26];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            letters[i] = (char)('a' + i);
+        }
 
+        for (int j = 0; j < input.Length; j++)
+        {
+            Console.WriteLine(Array.IndexOf(letters, input[j]));
+        }
     }
 }
