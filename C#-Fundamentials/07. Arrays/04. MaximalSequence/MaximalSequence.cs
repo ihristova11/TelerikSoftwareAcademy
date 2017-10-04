3a47e91ff7add1394a0e9b13d83326e8f702d438
Irina Hristova <hristova119@gmail.com>
2017-10-04 16:28:54 +0300

    Solved problem 04.

---
+++ C#-Fundamentials/07. Arrays/04. MaximalSequence/MaximalSequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.MaximalSequence
+{
+    class MaximalSequence
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxLen = 0, currLen = 1, prev;
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                prev = x;
+                x = int.Parse(Console.ReadLine());
+                //prev = x;
+                if(prev == x)
+                {
+                    currLen++;
+                    maxLen = (maxLen < currLen) ? currLen : maxLen;
+                }
+                else
+                {
+                    currLen = 1;
+                }
+            }
+            Console.WriteLine(maxLen);
+        }
+    }
+}
