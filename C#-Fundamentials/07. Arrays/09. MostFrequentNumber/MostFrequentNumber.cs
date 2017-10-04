e3a5623fbf4b75201fa7469f566659b990d312e4
Irina Hristova <hristova119@gmail.com>
2017-10-04 20:47:37 +0300

    Solved problem 09.

--- C#-Fundamentials/07. Arrays/09. MostFrequentNumber/MostFrequentNumber.cs
+++ C#-Fundamentials/07. Arrays/09. MostFrequentNumber/MostFrequentNumber.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace _09.MostFrequentNumber
 {
-    class Program
+    class MostFrequentNumber
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int times = 1;
+            int maxTimes = int.MinValue;
+            int mostFrequent = 1;
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            for (int j = 1; j < n; j++)
+            {
+                if(arr[j -1] == arr[j])
+                {
+                    times++;
+                    if(maxTimes < times)
+                    {
+                        maxTimes = times;
+                        mostFrequent = arr[j];
+                    }
+                }
+                else
+                {
+                    times = 0;
+                }
+            }
+            Console.WriteLine("{0} ({1} times)", mostFrequent, maxTimes + 1);
         }
     }
 }
