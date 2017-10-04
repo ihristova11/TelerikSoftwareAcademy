7f0d9d6ec793dda4ce85d43f0d6cc51108e1a0f5
Irina Hristova <hristova119@gmail.com>
2017-10-04 20:08:03 +0300

    Solved problem 07.

--- C#-Fundamentials/07. Arrays/07. SelectionSort/SelectionSort.cs
+++ C#-Fundamentials/07. Arrays/07. SelectionSort/SelectionSort.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _07.SelectionSort
 {
-    class Program
+    class SelectionSort
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            int temp;
+            for(int i = 0; i < n;i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    if(numbers[j] > numbers[k])
+                    {
+                        temp = numbers[j];
+                        numbers[j] = numbers[k];
+                        numbers[k] = temp;
+                    }
+                }
+            }
+
+            for (int p = 0; p < n; p++)
+            {
+                Console.WriteLine(numbers[p]);
+            }
         }
     }
 }
