151bf4b6286814785e2af30d0208d34923512a9e
Irina Hristova <hristova119@gmail.com>
2017-10-04 20:26:00 +0300

    Changing 08.

--- C#-Fundamentials/07. Arrays/08. MaximalSum/MaximalSum.cs
+++ C#-Fundamentials/07. Arrays/08. MaximalSum/MaximalSum.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace _08.MaximalSum
 {
-    class Program
+    class MaximalSum
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            int maxSum = int.MinValue;
+            int sum = 0;
+            int temp;
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
