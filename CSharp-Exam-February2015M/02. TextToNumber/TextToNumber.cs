d5e031132c3bfb649731fda38d15273c16e51106
Irina Hristova <hristova119@gmail.com>
2017-10-05 14:38:19 +0300

    Solved problem 02.

--- CSharp-Exam-February2015M/02. TextToNumber/TextToNumber.cs
+++ CSharp-Exam-February2015M/02. TextToNumber/TextToNumber.cs
@@ -4,6 +4,34 @@ class TextToNumber
 {
     static void Main()
     {
+        int m = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+        char[] arr = text.ToCharArray();
+        int result = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '@')
+            {
+                break;
+            }
+            else if (text[i] >= '0' && text[i] <= '9')
+            {
+                result *= int.Parse(text[i].ToString());
+            }
+            else if (text[i] >= 'a' && text[i] <= 'z')
+            {
+                result += (text[i] - 'a');
+            }
+            else if (text[i] >= 'A' && text[i] <= 'Z')
+            {
+                result += (text[i] - 'A');
+            }
+            else
+            {
+                result %= m;
+            }
+        }
+        Console.WriteLine(result);
     }
 }
 
