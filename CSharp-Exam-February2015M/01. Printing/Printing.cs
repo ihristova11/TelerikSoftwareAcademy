bdb754a01884caa22c00219f8b4329dd7ad46ad7
Irina Hristova <hristova119@gmail.com>
2017-10-05 13:21:30 +0300

    Solving problem.

--- CSharp-Exam-February2015M/01. Printing/Printing.cs
+++ CSharp-Exam-February2015M/01. Printing/Printing.cs
@@ -4,6 +4,9 @@ class Printing
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine()); //number of students in the academy
+        int s = int.Parse(Console.ReadLine()); //sheets that should be printed for a student
+        int p = int.Parse(Console.ReadLine()); //price of one realm (box with paper sheets)
     }
 }
 
