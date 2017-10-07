ddf6b27ae25735c61859ad84c407a75f21322c73
Irina Hristova <hristova119@gmail.com>
2017-10-07 20:42:30 +0300

    Solving problem.

--- CSharp-Exam-February2015M/04. Cube/Cube.cs
+++ CSharp-Exam-February2015M/04. Cube/Cube.cs
@@ -4,6 +4,14 @@ class Cube
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(new string(' ', n - 1) + new string(':', n)); // print the first line
+        for (int i = 0; i < n-2; i++)
+        {
+            Console.WriteLine(new string(' ', (n - 2 - i)) + ":" + 
+                new string('/', n - 2) + ":" + new string('X', i) + ":");
+        }
     }
 }
 
