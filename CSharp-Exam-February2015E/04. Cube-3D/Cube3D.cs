6a3570e65f52f900b7beefec45d4a66f60e12280
Irina Hristova <hristova119@gmail.com>
2017-10-07 21:18:08 +0300

    Solved problem.

--- CSharp-Exam-February2015E/04. Cube-3D/Cube3D.cs
+++ CSharp-Exam-February2015E/04. Cube-3D/Cube3D.cs
@@ -4,6 +4,19 @@ class Cube3D
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(new string(':', n));
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.WriteLine(":" + new string(' ', n - 2) + ":" + new string('|', i) + ":");
+        }
+        Console.WriteLine(new string(':', n) + new string('|', n - 2) + ":");
+        for (int j = 0; j < n - 2; j++)
+        {
+            Console.WriteLine(new string(' ', j + 1) + ":" +
+                new string('-', n - 2) + ":" + new string('|', n - 3 - j) + ":");
+        }
+        Console.WriteLine(new string(' ', n - 1) + new string(':', n));
     }
 }
 
