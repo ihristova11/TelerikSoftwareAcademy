eafdf6be5cb10e347a991636aac6c35c5b614cdb
Irina Hristova <hristova119@gmail.com>
2017-10-07 16:18:46 +0300

    Making changes.

--- CSharp-Exam-February2015M/03. SaddyKopper/SaddyKopper.cs
+++ CSharp-Exam-February2015M/03. SaddyKopper/SaddyKopper.cs
@@ -8,13 +8,21 @@ class SaddyKopper
         //StringBuilder number = new StringBuilder(Console.ReadLine());
 
         //Console.WriteLine(number);
-
+        TransformNumber();
 
     }
 
     static void TransformNumber()
     {
         StringBuilder number = new StringBuilder(Console.ReadLine());
+        number.Remove(number.Length - 1, 1);
+        Console.WriteLine(number + " " + number.Length);
+        int sum = 0, product = 1, transformations = 1;
+
+        for (int i = 0; i < number.Length; i+=2)
+        {
+
+        }
     }
 }
 
