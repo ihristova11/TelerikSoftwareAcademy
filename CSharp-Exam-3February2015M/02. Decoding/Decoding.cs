394260be2dfc84479f4afe54e1bd4a32fe08234f
Irina Hristova <hristova119@gmail.com>
2017-10-08 00:04:55 +0300

    Added new solution.

---
+++ CSharp-Exam-3February2015M/02. Decoding/Decoding.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Decoding
+{
+    static void Main()
+    {
+        int salt = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+        decimal encodedChar = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '@')
+            {
+                break;
+            }
+            else if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+            {
+                encodedChar += text[i] * salt + 1000;
+            }
+            else if(text[i] >= '0' && text[i] <= '9')
+            {
+                encodedChar += text[i] + salt + 500;
+            }
+
+            if(i % 2 == 0)
+            {
+                encodedChar = Math.Round(encodedChar / 100, 2);
+            }
+            else
+            {
+                encodedChar *= 100;
+            }
+            Console.WriteLine(encodedChar);
+        }
+    }
+}
+
