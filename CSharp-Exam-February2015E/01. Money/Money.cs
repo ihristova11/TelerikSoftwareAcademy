b77d643676e8c858cbe69dcae84752217c7929f4
Irina Hristova <hristova119@gmail.com>
2017-10-07 18:52:42 +0300

    Solved new problem.

---
+++ CSharp-Exam-February2015E/01. Money/Money.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01.Money
+{
+    class Money
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:f3}", (n * s * p / 400));
+        }
+    }
+}
