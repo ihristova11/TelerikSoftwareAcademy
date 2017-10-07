cbbcab99775882ec26f23f3d06ac52df9d419a79
Irina Hristova <hristova119@gmail.com>
2017-10-07 19:44:43 +0300

    Problem solved.

--- CSharp-Exam-February2015E/03. Maslan/Maslan.cs
+++ CSharp-Exam-February2015E/03. Maslan/Maslan.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace _03.Maslan
 {
@@ -7,22 +8,42 @@ namespace _03.Maslan
         static void Main()
         {
             string number = Console.ReadLine(); // step 1
-
-            while(number.Length > 1)
+            int transformations = 0;
+            BigInteger product = 1;
+            bool transformed = true;
+            while (number.Length > 1)
             {
-                number = number.Substring(0, number.Length - 1); // step 2 
-
-                int sum = 0;
+                product = 1;
+                //number = number.Substring(0, number.Length - 1); // step 2                                
                 int position = 0;
+                int sum = 0;
                 foreach (var symbol in number)
-                {
-                    if(position % 2 == 1)
+                {                  
+                    
+                    if (position % 2 == 1)
                     {
                         sum += symbol - '0';
                     }
+                    product *= sum != 0 ? sum : 1;
                     position++;
                 }
+                product /= sum;
+                transformations++;
+                number = product.ToString();
+
+                if(transformations == 10)
+                {
+                    transformed = false;
+                    break;
+                }
             }
+
+            if(transformed)
+            {
+                Console.WriteLine(transformations);
+            }
+
+            Console.WriteLine(number);
         }
     }
 }
