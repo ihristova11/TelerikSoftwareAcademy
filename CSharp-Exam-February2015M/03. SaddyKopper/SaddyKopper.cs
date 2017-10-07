c8829de0988ae5f7381b23db5a465e85069ea308
Irina Hristova <hristova119@gmail.com>
2017-10-07 17:37:57 +0300

    Making changes.

--- CSharp-Exam-February2015M/03. SaddyKopper/SaddyKopper.cs
+++ CSharp-Exam-February2015M/03. SaddyKopper/SaddyKopper.cs
@@ -4,22 +4,17 @@ using System.Text;
 
 class SaddyKopper
 {
-    public static object BigInteger { get; private set; }
-
-    static void Main()
+    public static void Main()
     {
         string number = Console.ReadLine();
-        //number = number.Substring(number.Length - 1, 1);
-        BigInteger product = 1;
-
-        int sum = 0, transformations = 0;
+        int transformations = 0;
         while (number.Length > 1 && transformations < 10)
         {
-            product = 1;
+            BigInteger product = 1;
             while (number.Length > 0)
             {
-                number.Remove(number.Length - 1, 1);
-                sum = 0;
+                number.Substring(0, number.Length - 1);
+                int sum = 0;
                 for (int i = 0; i < number.Length; i += 2)
                 {
                     sum += int.Parse(number[i].ToString());
@@ -29,9 +24,6 @@ class SaddyKopper
             transformations++;
             number = product.ToString();
         }
-
-
-
         if (transformations < 10)
         {
             Console.WriteLine(transformations);
