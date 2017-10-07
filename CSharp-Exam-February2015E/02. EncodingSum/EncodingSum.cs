c060206290b2da52d38c30194405d20949622933
Irina Hristova <hristova119@gmail.com>
2017-10-07 19:18:11 +0300

    Added new problem.

---
+++ CSharp-Exam-February2015E/02. EncodingSum/EncodingSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02.EncodingSum
+{
+    class EncodingSum
+    {
+        static void Main()
+        {
+            int m = int.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+            long result = 0;
+            text.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '@')
+                {
+                    Console.WriteLine(result);
+                    return;
+                }
+                else if (text[i] >= '0' && text[i] <= '9')
+                {
+                    result *= (text[i] - '0');
+                }
+                else if (text[i] >= 'a' && text[i] <= 'z')
+                {
+                    result += (text[i] - 'a');
+                }
+                else
+                {
+                    result %= m;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
