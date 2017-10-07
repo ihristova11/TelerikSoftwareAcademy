bdf166d47c0196d0e50d34f70176721937d0b95c
Irina Hristova <hristova119@gmail.com>
2017-10-07 16:15:41 +0300

    Solving problems

--- CSharp-Exam-February2015M/03. SaddyKopper/SaddyKopper.cs
+++ CSharp-Exam-February2015M/03. SaddyKopper/SaddyKopper.cs
@@ -5,10 +5,16 @@ class SaddyKopper
 {
     static void Main()
     {
+        //StringBuilder number = new StringBuilder(Console.ReadLine());
+
+        //Console.WriteLine(number);
+
+
+    }
+
+    static void TransformNumber()
+    {
         StringBuilder number = new StringBuilder(Console.ReadLine());
-
-        Console.WriteLine(number);
-
     }
 }
 
