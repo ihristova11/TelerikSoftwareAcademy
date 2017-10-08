5542c050f60cce0615d02fb18a234229597d1d19
Irina Hristova <hristova119@gmail.com>
2017-10-09 00:12:50 +0300

    Solving problems.

--- CSharp-Exam-3February2015M/04. PersianRugs/PersianRugs.cs
+++ CSharp-Exam-3February2015M/04. PersianRugs/PersianRugs.cs
@@ -6,6 +6,10 @@ class PersianRugs
     {
         int n = int.Parse(Console.ReadLine());
         int d = int.Parse(Console.ReadLine());
+        if (d > 2 * n - 3)
+        {
+            d = 0;
+        }
     }
 }
 
