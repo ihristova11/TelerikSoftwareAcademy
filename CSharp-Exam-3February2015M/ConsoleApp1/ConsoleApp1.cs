1cf9748d02aee9e3445779f83ab061ea12cbb7f8
Irina Hristova <hristova119@gmail.com>
2017-10-08 16:24:08 +0300

    Used BigInteger

--- CSharp-Exam-3February2015M/ConsoleApp1/ConsoleApp1.cs
+++ CSharp-Exam-3February2015M/ConsoleApp1/ConsoleApp1.cs
@@ -7,7 +7,7 @@ class ConsoleApp1
     {
         BigInteger product = 1;
         string input = Console.ReadLine();
-        int num;
+        BigInteger num;
         int counter = 1;
         int position = 0;
         int digitsProduct = 1;
@@ -17,12 +17,12 @@ class ConsoleApp1
             {
                 if (position % 2 == 1)
                 {
-                    num = int.Parse(input);
+                    num = BigInteger.Parse(input);
                     if (num == 0) digitsProduct = 1;
                     int digit;
                     while (num != 0)
                     {
-                        digit = num % 10;
+                        digit = int.Parse((num % 10).ToString());
                         if (digit == 0) digit = 1;
                         digitsProduct *= digit;
                         num /= 10;
