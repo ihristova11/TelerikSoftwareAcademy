8728f453fdf5b9984bf3fac2c256b585ba2cd267
Irina Hristova <hristova119@gmail.com>
2017-10-08 16:05:36 +0300

    Fixing bugs.

--- CSharp-Exam-3February2015M/ConsoleApp1/ConsoleApp1.cs
+++ CSharp-Exam-3February2015M/ConsoleApp1/ConsoleApp1.cs
@@ -1,9 +1,47 @@
 ﻿using System;
+using System.Numerics;
 
 class ConsoleApp1
 {
-    static void Main()
+    public static void Main()
     {
+        BigInteger product = 1;
+        string input = Console.ReadLine();
+        int num;
+        int counter = 1;
+        int position = 0;
+        int digitsProduct = 1;
+        while (input != "END")
+        {
+            if (counter <= 10)
+            {
+                if (position % 2 == 1)
+                {
+                    num = int.Parse(input);
+                    if (num == 0) digitsProduct = 1;
+                    int digit;
+                    while (num != 0)
+                    {
+                        digit = num % 10;
+                        if (digit == 0) digit = 1;
+                        digitsProduct *= digit;
+                        num /= 10;
+                    }
+                    product *= digitsProduct;
+                    digitsProduct = 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine(product);
+                product = 1;
+                counter -= 10;
+            }
+            input = Console.ReadLine();
+            counter++;
+            position++;
+        }
 
+        Console.WriteLine(product);
     }
 }
