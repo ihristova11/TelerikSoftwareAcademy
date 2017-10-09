695a5833140d9c57aa71ddb7052b7fbfde16710c
Irina Hristova <hristova119@gmail.com>
2017-10-09 13:46:34 +0300

    Solving problem.

--- CSharp-ExamJune2013M/02. TheSecretOfNumbers/TheSecretOfNumbers.cs
+++ CSharp-ExamJune2013M/02. TheSecretOfNumbers/TheSecretOfNumbers.cs
@@ -1,9 +1,34 @@
 ﻿using System;
+using System.Numerics;
 
 class TheSecretOfNumbers
 {
     static void Main()
     {
+        string number = Console.ReadLine();
+        int specialSum = 0;
+        bool hasSpecialSum = true;
+        string secretAlphaSequence = "";
+        for (int i = number.Length - 1; i >= 0; i--)
+        {
+            if ((number.Length - i) % 2 == 1) //odd positions case
+            {
+                specialSum += int.Parse(number[i].ToString()) * i * i;
+            }
+            else //even positions case 
+            {
+                specialSum += i * int.Parse(number[i].ToString()) * int.Parse(number[i].ToString());
+            }
+        }
+        int lastDigit = specialSum %= 10;
+        if(lastDigit == 0)
+        {
+            hasSpecialSum = false;
+        }
+        else
+        {
+            int r = specialSum % 26;
+        }
     }
 }
 
