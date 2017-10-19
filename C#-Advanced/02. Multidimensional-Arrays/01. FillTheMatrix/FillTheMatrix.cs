a822b2265c132f3bb1bba610fb023aae5cf00647
Irina Hristova <hristova119@gmail.com>
2017-10-19 21:30:53 +0300

    Solved for case a)

--- C#-Advanced/02. Multidimensional-Arrays/01. FillTheMatrix/FillTheMatrix.cs
+++ C#-Advanced/02. Multidimensional-Arrays/01. FillTheMatrix/FillTheMatrix.cs
@@ -6,10 +6,21 @@ class FillTheMatrix
     {
         int n = int.Parse(Console.ReadLine());
         string way = Console.ReadLine();
+        int[,] matrix = new int[n, n];
 
-        switch(way)
+        int counter = 1;
+        switch (way)
         {
             case "a":
+                for (int col = 0; col < matrix.GetLength(0); col++)
+                {
+                    for (int row = 0; row < matrix.GetLength(1); row++)
+                    {
+                        matrix[row, col] = counter;
+                        counter++;
+                    }
+                }
+
                 break;
 
             case "b":
@@ -23,6 +34,15 @@ class FillTheMatrix
 
             default:
                 break;
+        }
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                Console.Write(string.Format("{0} ", matrix[row, col]));
+            }
+            Console.WriteLine();
         }
     }
 }
