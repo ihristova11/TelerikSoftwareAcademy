d9d50f4e7eb45e9360cc00b6c6b59ddb82d44dd3
Irina Hristova <hristova119@gmail.com>
2017-10-19 21:09:07 +0300

    Solving problem

--- C#-Advanced/02. Multidimensional-Arrays/01. FillTheMatrix/FillTheMatrix.cs
+++ C#-Advanced/02. Multidimensional-Arrays/01. FillTheMatrix/FillTheMatrix.cs
@@ -4,6 +4,26 @@ class FillTheMatrix
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        string way = Console.ReadLine();
+
+        switch(way)
+        {
+            case "a":
+                break;
+
+            case "b":
+                break;
+
+            case "c":
+                break;
+
+            case "d":
+                break;
+
+            default:
+                break;
+        }
     }
 }
 
