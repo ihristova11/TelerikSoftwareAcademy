e0ca45da482637fe73dc2ca6a9f9d2362f0e2eae
Irina Hristova <hristova119@gmail.com>
2017-11-15 13:42:22 +0200

    Solved problem

--- C#-Advanced/04. NumeralSystems/04. HexadecimalToDecimal/HexadecimalToDecimal.cs
+++ C#-Advanced/04. NumeralSystems/04. HexadecimalToDecimal/HexadecimalToDecimal.cs
@@ -6,6 +6,48 @@
     {
         static void Main()
         {
+            //reading hexadecimal
+            string hex = Console.ReadLine();
+            long dec = Convert.ToInt64(hex, 16);
+            // converting to dec
+            //long dec = 0;
+            //int k = 0;
+            //for (int i = hex.Length - 1; i >= 0; i--)
+            //{
+            //    if(i >= 0 && i <= 9)
+            //    {
+            //        dec += long.Parse(Math.Pow(16, i).ToString()) * long.Parse(hex[i].ToString());
+            //    }
+            //    else
+            //    {
+            //        switch(hex[i])
+            //        {
+            //            case 'A':
+            //                k = 10;
+            //                break;
+            //            case 'B':
+            //                k = 11;
+            //                break;
+            //            case 'C':
+            //                k = 12;
+            //                break;
+            //            case 'D':
+            //                k = 13;
+            //                break;
+            //            case 'E':
+            //                k = 14;
+            //                break;
+            //            case 'F':
+            //                k = 15;
+            //                break;
+            //            default: break;
+            //        }
+
+            //        dec += long.Parse(Math.Pow(16, i).ToString()) * long.Parse(k.ToString());
+            //    }
+            //}
+
+            Console.WriteLine(dec);
         }
     }
 }
