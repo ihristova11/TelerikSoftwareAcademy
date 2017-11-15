c871da3e214cd2d97ddf085d9d31bdd60cb3da57
Irina Hristova <hristova119@gmail.com>
2017-11-15 14:00:40 +0200

    Solving problem.

--- C#-Advanced/04. NumeralSystems/07. OneSystemToAnyOther/OneSystemToAnyOther.cs
+++ C#-Advanced/04. NumeralSystems/07. OneSystemToAnyOther/OneSystemToAnyOther.cs
@@ -6,6 +6,14 @@
     {
         static void Main()
         {
+            int s = int.Parse(Console.ReadLine());
+            string n = Console.ReadLine();
+            int d = int.Parse(Console.ReadLine());
+        }
+
+        public static void ConvertToDecimal(int baseF, int baseS, string n)
+        {
+            long dec = Convert.ToInt64(n, baseF);
         }
     }
 }
