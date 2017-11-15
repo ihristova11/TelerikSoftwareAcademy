b7e38dd6c469e0d30c72ce467207a5e6de67541d
Irina Hristova <hristova119@gmail.com>
2017-11-15 13:23:57 +0200

    Solved problem.

--- C#-Advanced/04. NumeralSystems/02. BinaryToDecimal/BinaryToDecimal.cs
+++ C#-Advanced/04. NumeralSystems/02. BinaryToDecimal/BinaryToDecimal.cs
@@ -1,11 +1,15 @@
 ﻿namespace _02.BinaryToDecimal
 {
     using System;
+    using System.Text;
 
     class BinaryToDecimal
     {
         static void Main()
         {
+            string binary = Console.ReadLine();
+            long l = Convert.ToInt64(binary, 2);
+            Console.WriteLine(l);
         }
     }
 }
