436acb80fa70e9802ea29049e2f71ee7c50373cb
Irina Hristova <hristova119@gmail.com>
2017-11-15 13:12:44 +0200

    Solved problem.

--- C#-Advanced/04. NumeralSystems/01. DecimalToBinary/DecimalToBinary.cs
+++ C#-Advanced/04. NumeralSystems/01. DecimalToBinary/DecimalToBinary.cs
@@ -1,11 +1,30 @@
 ﻿namespace _01.DecimalToBinary
 {
     using System;
+    using System.Numerics;
+    using System.Text;
 
     class DecimalToBinary
     {
         static void Main()
         {
+            BigInteger n = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine(ToNBase(n, 2));
+
+            
+            
+        }
+
+        public static string ToNBase(BigInteger a, int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (a > 0)
+            {
+                sb.Insert(0, a % n);
+                a /= n;
+            }
+            return sb.ToString();
         }
     }
 }
