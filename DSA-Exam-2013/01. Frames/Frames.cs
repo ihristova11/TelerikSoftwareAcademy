a97015b0ede1c9be91aafe8ecf2b64c83d630c7b
Irina Hristova <hristova119@gmail.com>
2017-11-16 15:32:41 +0200

    Solving problem.

--- DSA-Exam-2013/01. Frames/Frames.cs
+++ DSA-Exam-2013/01. Frames/Frames.cs
@@ -6,6 +6,15 @@
     {
         static void Main()
         {
+            byte n = byte.Parse(Console.ReadLine());
+            byte differentFrames = n;
+            byte numberInLine = byte.Parse((n - 1).ToString());
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+            }
+            //Console.WriteLine(numberInLine);
         }
     }
 }
