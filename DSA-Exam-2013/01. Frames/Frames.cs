8b265c63c8c34e711f039653b601d4a72c044832
Irina Hristova <hristova119@gmail.com>
2017-11-18 12:26:57 +0200

    Solving problem.

--- DSA-Exam-2013/01. Frames/Frames.cs
+++ DSA-Exam-2013/01. Frames/Frames.cs
@@ -8,12 +8,38 @@
         {
             byte n = byte.Parse(Console.ReadLine());
             byte differentFrames = n;
+            string[] lines = new string[n];
             byte numberInLine = byte.Parse((n - 1).ToString());
+            int result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                lines[i] = Console.ReadLine();
+            }
 
             for (int i = 0; i < n; i++)
             {
-                string line = Console.ReadLine();
+                for (int j = i + 1; j < n; j++)
+                {
+                    if(lines[i] == lines[j])
+                    {
+                        differentFrames--;
+                    }
+                }
             }
+
+            if (differentFrames == 0) differentFrames = 1;
+
+            for (byte i = differentFrames; i >= 1; i--)
+            {
+                result *= i;
+            }
+
+            for (int j = n - 1; j >= 1; j--)
+            {
+                result *= j;
+            }
+            //Console.WriteLine(differentFrames);
+            Console.WriteLine(result);
             //Console.WriteLine(numberInLine);
         }
     }
