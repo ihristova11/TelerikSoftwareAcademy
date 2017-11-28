b5c386e17ceae0430db42eeabbbe0c57bbf5991c
Irina Hristova <hristova119@gmail.com>
2017-11-28 22:16:32 +0200

    Solved problem 100/100.

--- CSharpAdvancedExam1June2016/02. Kitty/Kitty.cs
+++ CSharpAdvancedExam1June2016/02. Kitty/Kitty.cs
@@ -16,29 +16,95 @@
         static void Main()
         {
             path = new StringBuilder(Console.ReadLine());
-            directions = Console.ReadLine()
+            directions = ("0 " + Console.ReadLine())
                 .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
 
-
+            CheckPosition();
         }
 
         public static void CheckPosition()
         {
             for (int direction = 0; direction < directions.Length; direction++)
             {
-                position += directions[direction];
-
-                if(position < 0)
+                int move = directions[direction];
+                if (move > 0) //right
                 {
-                    position += path.Length;
+                    position += move;
+                    while (position > path.Length - 1)
+                    {
+                        position -= path.Length;
+                    }
                 }
-                else if(position > path.Length - 1)
+
+                if (move < 0) //left
                 {
-                    position -= path.Length;
+                    position += move;
+                    while (position < 0)
+                    {
+                        position += path.Length;
+                    }
+                }
+               // position += directions[direction];
+
+                //if (position < 0)
+                //{
+                //    position += path.Length;
+                //}
+                //else if(position > path.Length - 1)
+                //{
+                //    position -= path.Length;
+                //}
+
+                switch(path[position])
+                {
+                    case '@':
+                        souls++;
+                        path[position] = 'v';
+                        break;
+                    case '*':
+                        food++;
+                        path[position] = 'v';
+                        break;
+                    case 'x':
+                        if(position % 2 == 0)
+                        {
+                            if (souls > 0)
+                            {
+                                souls--;
+                                deadlocks++;
+                                path[position] = '@';
+                            }
+                            else
+                            {
+                                Console.WriteLine("You are deadlocked, you greedy kitty!");
+                                Console.WriteLine("Jumps before deadlock: {0}", direction);
+                                return;
+                            }
+                        }
+                        else 
+                        {
+                            if (food > 0) {
+                                food--;
+                                deadlocks++;
+                                path[position] = '*';
+                            }
+                            else
+                            {
+                                Console.WriteLine("You are deadlocked, you greedy kitty!");
+                                Console.WriteLine("Jumps before deadlock: {0}", direction);
+                                return;
+                            }
+                        }
+                        break;
+                    default: break;
                 }
             }
+
+            Console.WriteLine("Coder souls collected: {0}", souls);
+            Console.WriteLine("Food collected: {0}", food);
+            Console.WriteLine("Deadlocks: {0}", deadlocks);
         }
     }
 }
