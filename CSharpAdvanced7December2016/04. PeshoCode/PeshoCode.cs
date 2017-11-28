7ac4a6ffc0f5f541c2375b4dac62eae1be569354
Irina Hristova <hristova119@gmail.com>
2017-11-28 23:19:50 +0200

    Solving problem.

--- CSharpAdvanced7December2016/04. PeshoCode/PeshoCode.cs
+++ CSharpAdvanced7December2016/04. PeshoCode/PeshoCode.cs
@@ -7,7 +7,18 @@
         static void Main()
         {
             string word = Console.ReadLine();
+            
             int numberOfLines = int.Parse(Console.ReadLine());
+            string[] lines = new string[numberOfLines];
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                lines[i] = Console.ReadLine();
+            }
+
+            string text = string.Join(" ", lines);
+
+            //Console.WriteLine(text);
         }
     }
 }
