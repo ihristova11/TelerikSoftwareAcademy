6557d503a1a092126fd8f0b8dace6ea1367eb182
Irina Hristova <hristova119@gmail.com>
2017-11-28 21:49:50 +0200

    Solving problem.

--- CSharpAdvancedExam1June2016/02. Kitty/Kitty.cs
+++ CSharpAdvancedExam1June2016/02. Kitty/Kitty.cs
@@ -6,72 +6,39 @@
 
     class Kitty
     {
+        static int position = 0;
+        static int direction = 0;
+        static int souls = 0;
+        static int deadlocks = 0;
+        static int food = 0;
+        static int[] directions;
+        static StringBuilder path;
         static void Main()
         {
-            StringBuilder path = new StringBuilder(Console.ReadLine());
-            int[] directions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            int position = 0;
-            int souls = 0, deadlocks = 0, food = 0;
-            //bool[] beenThere = new bool[directions.Length];
+            path = new StringBuilder(Console.ReadLine());
+            directions = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
+
+        }
+
+        public static void CheckPosition()
+        {
             for (int direction = 0; direction < directions.Length; direction++)
             {
-                if (position == 0) { direction = -1; }
-                else
-                {
-                    position += directions[direction];
-                }
+                position += directions[direction];
 
-                //check if index out of range
-                if (position < 0)
+                if(position < 0)
                 {
-                    position += (path.Length - 1);
+                    position += path.Length;
                 }
-                else if (position > path.Length - 1)
+                else if(position > path.Length - 1)
                 {
-                    position -= (path.Length - 1);
-                }
-
-                switch (path[position])
-                {
-                    case '@':
-                        souls++;
-                        path[position] = 'v';
-                        break;
-                    case 'x':
-                        deadlocks++;
-                        if (position % 2 == 0 && souls > 0)
-                        {
-                            souls--;
-                            deadlocks++;
-                            path[position] = 'v';
-                        }
-                        else if (position % 2 == 1 && food > 0)
-                        {
-                            food--;
-                            deadlocks++;
-                            path[position] = 'v';
-                        }
-                        else
-                        {
-                            Console.WriteLine("You are deadlocked, you greedy kitty!");
-                            Console.WriteLine("Jumps before deadlock: {0}", direction);
-                            return;
-                        }
-                        break;
-                    case '*':
-                        food++;
-                        path[position] = 'v';
-                        break;
-                    default:
-                        throw new ArgumentException("ERROR");
-                        break;
+                    position -= path.Length;
                 }
             }
-            Console.WriteLine("Coder souls collected: {0}", souls);
-            Console.WriteLine("Food collected: {0}", food);
-            Console.WriteLine("Deadlocks: {0}", deadlocks);
-
         }
     }
 }
