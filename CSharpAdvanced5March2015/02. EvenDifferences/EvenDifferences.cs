ffe5b62704faba15137bcad6e36d6c03ed22a34c
Irina Hristova <hristova119@gmail.com>
2017-11-29 20:57:40 +0200

    Solving problems.

--- CSharpAdvanced5March2015/02. EvenDifferences/EvenDifferences.cs
+++ CSharpAdvanced5March2015/02. EvenDifferences/EvenDifferences.cs
@@ -1,11 +1,16 @@
 ﻿namespace _02.EvenDifferences
 {
     using System;
+    using System.Linq;
 
     class EvenDifferences
     {
         static void Main()
         {
+            int[] numbers = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
         }
     }
 }
