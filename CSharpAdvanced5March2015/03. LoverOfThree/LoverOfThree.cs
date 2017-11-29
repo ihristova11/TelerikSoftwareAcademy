7d4d1b7af1bae9debc5840d017c5269d5809c7c4
Irina Hristova <hristova119@gmail.com>
2017-11-29 22:26:27 +0200

    Solving problem.

--- CSharpAdvanced5March2015/03. LoverOfThree/LoverOfThree.cs
+++ CSharpAdvanced5March2015/03. LoverOfThree/LoverOfThree.cs
@@ -5,8 +5,34 @@
     class LoverOfThree
     {
         static int[,] matrix;
-        static int rows;
-        static int cols;
+        static int[] rows = { -1, -1, 1, 1 };
+        static int[] cols = { -1, 1, 1, -1 };
+
+        static int GetMoveDirection(string direction)
+        {
+            switch(direction)
+            {
+                case "LU": return 0;
+                    break;
+                case "UL": return 0;
+                    break;
+                case "RU":return 1;
+                    break;
+                case "UR":return 1;
+                    break;
+                case "RD":return 2;
+                    break;
+                case "DR":return 2;
+                    break;
+                case "LD":return 3;
+                    break;
+                case "DL":return 3;
+                    break;
+
+                default: return;
+            }
+        }
+
         static void Main()
         {
         }
