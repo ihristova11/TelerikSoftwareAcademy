b45e2ab2d4b83e2b880a36337f257f535bebed71
Irina Hristova <hristova119@gmail.com>
2017-12-02 00:19:15 +0200

    Solving problem.

--- CSharpAdvanced31May2016/02. DancingMoves/DancingMoves.cs
+++ CSharpAdvanced31May2016/02. DancingMoves/DancingMoves.cs
@@ -6,6 +6,7 @@
     class DancingMoves
     {
         public static string line;
+        public static int sum = 0;
         static void Main()
         {
             //reading the input
@@ -31,6 +32,32 @@
                 int repeats = int.Parse(instructions[0]);
                 string direction = instructions[1];
                 int steps = int.Parse(instructions[2]);
+
+                int position = 0;
+
+                for (int i = 1; i <= repeats; i++)
+                {
+                    if (direction == "left")
+                    {
+                        position -= steps;
+                    }
+                    else if(direction == "right")
+                    {
+                        position += steps;
+                    }
+
+                    //if position is out of boundaries
+                    while(position < 0)
+                    {
+                        position += arr.Length;
+                    }
+
+                    while(position > arr.Length - 1)
+                    {
+                        position -= arr.Length;
+                    }
+
+                }
             }
 
         }
