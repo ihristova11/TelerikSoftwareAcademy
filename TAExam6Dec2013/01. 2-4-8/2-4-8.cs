294adc52cb93ef6eaad41e728ce2798cbaaf3315
Irina Hristova <hristova119@gmail.com>
2018-01-05 23:51:52 +0200

    Added solutions to exams

---
+++ TAExam6Dec2013/01. 2-4-8/2-4-8.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01._2_4_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            ulong a = ulong.Parse(Console.ReadLine());
+            ulong b = ulong.Parse(Console.ReadLine());
+            ulong c = ulong.Parse(Console.ReadLine());
+            //int prod = 0;
+            ulong r = 0;
+            switch (b)
+            {
+                case 2:
+                    r = a % c;
+                    break;
+                case 4:
+                    r = a + c;
+                    break;
+                case 8:
+                    r = a * c;
+                    break;
+                default:
+                    break;
+            }
+
+            if (r % 4 == 0)
+            {
+                Console.WriteLine(r / 4);
+                Console.WriteLine(r);
+            }
+            else
+            {
+                Console.WriteLine(r % 4);
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
---
+++ TAExam6Dec2013/02. NightmareOnCodeStreet/NightmareOnCodeStreet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.NightmareOnCodeStreet
+{
+    class NightmareOnCodeStreet
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int len = input.Length;
+            int counter = 0;
+            int sum = 0;
+            int digit;
+            bool check = true;
+            for (int i = 1; i < len; i+=2)
+            {
+                check = int.TryParse(input[i].ToString(), out digit);
+                if(check)
+                {
+                    counter++;
+                    sum += int.Parse(input[i].ToString());
+                }
+                
+            }
+            Console.WriteLine(counter + " " + sum);
+        }
+    }
+}
